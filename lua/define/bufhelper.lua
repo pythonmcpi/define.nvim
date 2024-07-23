2d33d1f7c4ed4d8ef3659089e3cc2b87980e132d
Pipythonmc <47196755+pythonmcpi@users.noreply.github.com>
2024-07-22 23:15:49 -0700

    tweak: proper scratch buffer settings and change window position based on terminal size

--- lua/define/bufhelper.lua
+++ lua/define/bufhelper.lua
@@ -1,20 +1,17 @@
--- Based on https://stackoverflow.com/a/75240496
+-- Originallu based on https://stackoverflow.com/a/75240496
 
 local buffer_number = -1
 
 local function display(data)
     if data then
-        -- Make buffer writable
         vim.api.nvim_buf_set_option(buffer_number, "readonly", false)
 
-        -- Append data
+        -- Append instead of overwrite
         vim.api.nvim_buf_set_lines(buffer_number, -1, -1, true, data)
 
-        -- Make buffer readonly
         vim.api.nvim_buf_set_option(buffer_number, "readonly", true)
 
-        -- Mark buffer as not modified so there's no need to save when exiting
-        vim.api.nvim_buf_set_option(buffer_number, "modified", false)
+        -- Modification flag is never set when buftype = "nofile"
     end
 end
 
@@ -22,14 +19,26 @@ local function open_buffer(name)
     -- Check if the buffer number is still visible
     local buffer_visible = vim.api.nvim_call_function("bufwinnr", { buffer_number })
 
-    if buffer_number == -1 or not buffer_visible then
-        -- Create new buffer
-        vim.api.nvim_command("botright vsplit " .. name)
+    if buffer_number == -1 or buffer_visible == -1 then
+        -- Check aspect ratio of current window
+        local ratio = vim.o.columns / vim.o.lines
+
+        if ratio < 3 then
+            -- New buffer on bottom
+            vim.api.nvim_command("botright split " .. name)
+        else
+            -- New buffer on the right
+            vim.api.nvim_command("botright vsplit " .. name)
+        end
 
         buffer_number = vim.api.nvim_get_current_buf()
 
-        -- Mark buffer readonly
+        -- Set buffer flags
         vim.opt_local.readonly = true
+        vim.opt_local.buftype = "nofile"
+        vim.opt_local.bufhidden = "hide"
+        vim.opt_local.buflisted = false
+        vim.opt_local.swapfile = false
     end
 end
 
@@ -39,7 +48,6 @@ local function set_buffer(lines)
     vim.api.nvim_buf_set_option(buffer_number, "readonly", false)
     vim.api.nvim_buf_set_lines(buffer_number, 0, -1, true, lines)
     vim.api.nvim_buf_set_option(buffer_number, "readonly", true)
-    vim.api.nvim_buf_set_option(buffer_number, "modified", false)
 end
 
 local function clear_buffer()
